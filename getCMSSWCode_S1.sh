d3662f813be6d7221bed16a52fa0ad2d09ec17ad
Louis Portales <louis.portales@gmail.com>
2021-12-17 17:31:28 +0100

    more consistent git setup scripts

--- getCMSSWCode_S1.sh
+++ getCMSSWCode_S1.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 git init
-git remote add -t S2ClusteringEmulatorIntegration S2-cmssw git@github.com:EmyrClement/cmssw.git
 git remote add -t S1SortingTruncationEmulator S1-cmssw git@github.com:portalesHEP/cmssw.git
 git config core.sparseCheckout true
 echo L1Trigger/L1THGCal/interface/backend_emulator >> .git/info/sparse-checkout
 echo L1Trigger/L1THGCal/src/backend_emulator >> .git/info/sparse-checkout
-git pull S2-cmssw S2ClusteringEmulatorIntegration
 git pull S1-cmssw S1SortingTruncationEmulator
-#git checkout -b S1SortingTruncationEmulator
+git checkout -b S1SortingTruncationEmulator
