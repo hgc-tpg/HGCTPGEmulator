d7818db4f8cf7b72cb7c258f0db993d0aa2ae554
Louis Portales <louis.portales@gmail.com>
2022-06-13 17:28:26 +0200

    automatic evaluation of number of BX in TC list generation script

--- scripts/generateTClist.py
+++ scripts/generateTClist.py
@@ -3,18 +3,30 @@ import optparse
 from os import listdir
 from os.path import isfile, join
 import json
- 
-def read_inputs(input_dir):
+import yaml
+
+def read_inputs(input_dir, fw_config):
     stimuli = {}
     input_files = [f for f in listdir(input_dir) if isfile(join(input_dir,f))]
+    nbx = -1
     for f in input_files:
         modhash = "".join((filter(str.isdigit, f)))
-        stimuli[modhash] = np.loadtxt(
-            join(input_dir,f),
-            dtype = 'str',
-            usecols = range(0,48) # always 48 TCs per module
-        )
-    return stimuli
+        save_module = True
+        if fw_config:
+            with open(fw_config, 'r') as cfg_file:
+                params = yaml.safe_load(cfg_file)
+            if 'modules' in params:
+                save_module = (modhash in params['modules'])
+        if save_module:
+            stimuli[modhash] = np.loadtxt(
+                join(input_dir,f),
+                dtype = 'str',
+                usecols = range(0,48) # always 48 TCs per module
+            )
+            if nbx == -1:
+                nbx = len(stimuli[modhash])
+    return stimuli, nbx
+
 
 def order_per_bx(stimuli, Nbx):
     stimuli_per_bx = {'event' : []}
@@ -26,20 +38,22 @@ def order_per_bx(stimuli, Nbx):
             stimuli_per_bx['event'][bx]['module'][modhash]['tc'] = []
             for tc_e in stimuli[modhash][bx,:]:
                 stimuli_per_bx['event'][bx]['module'][modhash]['tc'].append(tc_e)
-    return stimuli_per_bx         
-   
+    return stimuli_per_bx
+
+
+def main(opt):
+    mod_stimuli, nbx = read_inputs(opt.input_dir, opt.fw_config)
+    mod_stimuli = order_per_bx(mod_stimuli, nbx)
+    output_file = opt.output_dir+'/TClist.json'
+    with open(output_file,'w') as f_out:
+        json.dump(mod_stimuli, f_out, indent=4)
+
 if __name__=='__main__':
+
     parser = optparse.OptionParser()
-    parser.add_option("--nbx", type="int",    dest="nbx",        help="Number of Bx in stimuli files")
-    parser.add_option("--in",  type="string", dest="input_dir",  help="Input directory")
+    parser.add_option("--fwcfg", type="string", dest="fw_config",  default="", help="(optional) yaml file with TC->bin mapping information")
+    parser.add_option("--in",  type="string", dest="input_dir", help="Input directory")
     parser.add_option("--out", type="string", dest="output_dir", help="Output directory")
     opt, args = parser.parse_args()
 
-    mod_stimuli = read_inputs(opt.input_dir)
-    mod_stimuli = order_per_bx(mod_stimuli, opt.nbx)
-    
-    output_file = opt.output_dir+'/TClist.json'
-    with open(output_file,'w') as f_out:    
-        json.dump(mod_stimuli, f_out, indent=4)
-
-        
+    main(opt)
