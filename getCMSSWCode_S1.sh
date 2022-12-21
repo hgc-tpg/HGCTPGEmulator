a5ea1b190fe1451355fc0cb5e43cc0e29cfcd841
Louis Portales <louis.portales@gmail.com>
2022-12-21 13:58:18 +0100

    now setting up CMSSW environment for C++ version consistency

--- getCMSSWCode_S1.sh
+++ getCMSSWCode_S1.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+CMSSW_VERSION=${1-CMSSW_12_3_0}
+
+# setup CMS environment to get required C++17 version
+export SCRAM_ARCH=slc7_amd64_gcc10
+cmsrel ${CMSSW_VERSION}
+cd ${CMSSW_VERSION}/src/
+cmsenv
+cd -
+
 git init
 git remote add -t S1EmulatorDevelopement_pre4 S1-cmssw https://github.com/portalesHEP/cmssw.git
 git config core.sparseCheckout true
@@ -6,3 +16,4 @@ echo L1Trigger/L1THGCal/interface/backend_emulator >> .git/info/sparse-checkout
 echo L1Trigger/L1THGCal/src/backend_emulator >> .git/info/sparse-checkout
 git pull S1-cmssw S1EmulatorDevelopement_pre4
 git checkout -b S1EmulatorDevelopement_pre4
+
